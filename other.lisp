35bebddb13e74a626d2c67a40f818b3bf8a0998f
terminal625 <zpersoncool@gmail.com>
2018-09-03 22:29:09 -0400

    ran the main, ran a real fib!

--- other.lisp
+++ other.lisp
@@ -25,9 +25,9 @@
 	      (k-shared::with-llvm-message (ptr) (cffi:mem-ref foo :pointer)
 		(format t "~%llvm parse failed:")
 		(princ (cffi:foreign-string-to-lisp ptr)))))
-	  (print 234234)
+	 ;; (print 234234)
 	  ;;(llvm::-dispose-memory-buffer memory-buffer) ;;;FIXME::no disposal?
-	  (print 234234)
+	  ;;(print 234234)
 	  (values
 	   (cffi:mem-ref module-pointer 'llvm::|LLVMModuleRef|)
 	   success))))))
@@ -37,7 +37,8 @@
   ((module :accessor module.module
 	   :initarg :module)
    (function-pass-manager :accessor module.function-pass-manager
-			  :initarg :function-pass-manager)))
+			  :initarg :function-pass-manager)
+   (live :accessor module.live :initarg :live)))
 (defun make-module (&key
 		      (name "no name")
 		      module)
@@ -52,17 +53,19 @@
 		   module
 		   :function-pass-manager
 		   function-pass-manager
-		   )))
+		   :live t)))
 
 (defun dispose-module (module)
-  (llvm::-dispose-pass-manager (module.function-pass-manager module))
-  (llvm::-dispose-module (module.module module))
+  (when (slot-value module 'live)
+    (llvm::-dispose-pass-manager (module.function-pass-manager module))
+    (llvm::-dispose-module (module.module module)))
   (values))
 
 (defmacro with-module ((&optional (name-form "a module")) name &body body)
   `(let ((,name (make-module ,name-form)))
      (unwind-protect (progn ,@body)
-       (dispose-module ,name))))
+       (when (slot-value ,name 'live)
+	 (dispose-module ,name)))))
 
 (defun %initialize-module-and-pass-manager (&optional (string "module name"))
   "return (values [NEW MODULE] [NEW FUNCTION PASS MANAGER])"
@@ -89,38 +92,56 @@
 	   (make-module :module
 			(read-ir-into-module expression))))
       (unwind-protect
-	   (with-slots (module) module-object
-	     
-	     (k-shared::dump-module module)
-	     (%bar module))
+	   (progn
+	     (with-slots (module) module-object
+	       (let ((module-abnormal? (k-shared::verify-llvm-module module)))
+		 ;; (k-shared::dump-module module)
+		 (if module-abnormal?
+		     nil
+		     (%bar module-object)))))
 	(dispose-module module-object)))))
 
+(cffi:define-foreign-library (libgc)
+  (t (:default "libgc")))
+
+(cffi:use-foreign-library libgc)
 
 ;;;;lf == (codegen ast)
-(defun %bar (module &optional (name "main")) ;;;Whats lf?
+(defun %bar (module-object &optional (name "main")) ;;;Whats lf?
   "Evalutate main"
   (declare (optimize (speed 0) (debug 3)))
-  (let ((module-abnormal? (verify-llvm-module module)))
-    (when module-abnormal?
-      (let ((handle (k-shared::kaleidoscope-add-module module)))
-	(unwind-protect
-	     (let ((expr-symbol
-		    (cffi:with-foreign-string (str name)
-		      (k-shared::kaleidoscope-find-symbol str))))
-	       (when (cffi:null-pointer-p expr-symbol)
-		 (error 'k-shared::kaleidoscope-error :message "function not found"))
-	       (let ((ptr (k-shared::kaleidoscope-get-symbol-address expr-symbol)))
-		 (if (= 0 ptr)
-		     (error 'k-shared::kaleidoscope-error :message "function no body???")
-		     (let ((result
-			    (cffi:foreign-funcall-pointer
-			     (cffi:make-pointer ptr) 
-			     ()
-			     (:pointer :int32)
-			     (cffi:null-pointer)
-			     (:pointer (:pointer :int8))
-			     (cffi:null-pointer)
-			     :int64)))
-		       (format t "~%Evaluated to ~fD0~%"
-			       result)))))
-	  (k-shared::kaleidoscope-remove-module handle))))))
+  (with-slots (module live) module-object   
+    (let ((handle (k-shared::kaleidoscope-add-module module)))
+      (unwind-protect
+	   (let ((expr-symbol
+		  (cffi:with-foreign-string (str name)
+		    (k-shared::kaleidoscope-find-symbol str))))
+	     (when (cffi:null-pointer-p expr-symbol)
+	       (error 'k-shared::kaleidoscope-error :message "function not found"))
+	     (let ((ptr (k-shared::kaleidoscope-get-symbol-address expr-symbol)))
+	       (if (= 0 ptr)
+		   (error 'k-shared::kaleidoscope-error :message "function no body???")
+		   (let ((result
+			  (cffi:foreign-funcall-pointer
+			   (cffi:make-pointer ptr) 
+			   ()
+			   (:pointer :int32)
+			   (cffi:null-pointer)
+			   (:pointer (:pointer :int8))
+			   (cffi:null-pointer)
+			   :int64)))
+		     (format t "~%Evaluated to ~fD0~%"
+			     result)))))
+	(k-shared::kaleidoscope-remove-module handle)
+	(setf live nil)))))
+
+(defun foobar ()
+  (bar '(begin
+	 (define (hoopla x y z)
+	  (z
+	   (* x y)))
+	 (define (fib x)
+	  (if (= x 0)
+	      1
+	      (+ x (fib (- x 1)))))
+	 (hoopla 3 4 fib))))
